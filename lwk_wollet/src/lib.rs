9c9a3264141e33b40c5a9903dc4bb04e3cb89557
Ross Savage <hello@satimoto.com>
2025-01-02 15:04:21 +0100

    wollet: full scan up to derivation index

--- lwk_wollet/src/lib.rs
+++ lwk_wollet/src/lib.rs
@@ -117,6 +117,8 @@ pub use crate::wollet::{Tip, Wollet};
 #[cfg(feature = "electrum")]
 pub use crate::wollet::full_scan_with_electrum_client;
 #[cfg(feature = "electrum")]
+pub use crate::wollet::full_scan_to_index_with_electrum_client;
+#[cfg(feature = "electrum")]
 pub use clients::electrum_client::{ElectrumClient, ElectrumOptions, ElectrumUrl};
 
 #[cfg(feature = "esplora")]
